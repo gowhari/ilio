8acafb83f7c16f70073c6f114afae441c536988a
iman gowhari <gowhari@gmail.com>
2017-09-20 22:59:42 +0430

    change version number to 0.4.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name='ilio',
-      version='0.3.0',
+      version='0.4.0',
       description='inline io: just one function call instead of file open(), read(), close()',
       long_description=open('README.rst').read(),
       url='https://github.com/gowhari/ilio',
